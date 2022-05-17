5f39f21999a69d7f04ed3f9382690606877f80fe
OmeAichi <91714263+OmelloPva@users.noreply.github.com>
2022-05-17 18:25:50 +0300

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -6,28 +6,33 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Aliens coming")
 
 x = 50
-y = 50
+y = 425
 widht = 40
 height = 60
 speed = 5
 
+isJump = False
+jumpCount = 10
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x >5:
+    if keys[pygame.K_LEFT] and x > 5:
         x -= speed
     if keys[pygame.K_RIGHT] and x < 500 - widht - 5:
         x += speed
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y > 5:
         y -= speed
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and y < 500 - height - 15:
         y += speed
+    if keys[pygame.K_SPACE]:
+        isJump = True
 
     win.fill((0,0,0))
     pygame.draw.rect(win, (153, 0, 0,), (x, y, widht, height))
