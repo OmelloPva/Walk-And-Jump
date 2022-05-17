faf32a2c80477f0608d7949667f145994b8c4d98
OmeAichi <91714263+OmelloPva@users.noreply.github.com>
2022-05-17 19:34:56 +0300

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -23,11 +23,11 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 5:
+        x -= speed
+    if keys[pygame.K_RIGHT] and x < 500 - widht - 5:
+        x += speed
     if not(isJump):
-        if keys[pygame.K_UP] and y > 5:
-            y -= speed
-        if keys[pygame.K_DOWN] and y < 500 - height - 15:
-            y +=speed
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
